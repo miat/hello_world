82126952fed278690d68eb0f61814bb021e7e972
Miat <miat@monkeeworks.com>
2022-03-20 21:45:57 -0400

    Create basic_coroutine.cpp

---
+++ basic_coroutine.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <coroutine>
+
+using namespace std;
+
+class Awaiter {
+  public:
+    std::coroutine_handle<> m_handle;
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<> handle_) { m_handle = handle_; }
+    void await_resume() { }
+
+    // return_value
+};
+
+struct ReturnObject : std::coroutine_handle<> {
+    std::coroutine_handle<> m_handle;
+    struct promise_type {
+        using Handle = std::coroutine_handle<promise_type>;
+
+        ReturnObject get_return_object() { return ReturnObject{Handle::from_promise(*this)}; }
+        std::suspend_never initial_suspend() { return {}; }
+        std::suspend_never final_suspend() noexcept { return {}; }
+        void return_void(){};
+        void unhandled_exception() { }
+    };
+    explicit ReturnObject(promise_type::Handle coro) : m_handle(coro) { }
+    void resume() { m_handle.resume(); }
+    void destroy() { m_handle.destroy(); }
+};
+
+ReturnObject counter() {
+    Awaiter a;
+    for (int i = 0;; ++i) {
+        co_await a;
+        cout << "counter: " << i << endl;
+    }
+    co_return;
+}
+
+int main() {
+    auto handle = counter();
+    for (int i = 0; i < 3; ++i) {
+        std::cout << "In main1 function\n";
+        handle.resume();
+    }
+    handle.destroy();
+}
