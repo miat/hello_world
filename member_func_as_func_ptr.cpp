a03d631ac9cc57a2e9e85c9649b114f7de209639
Miat <miat@monkeeworks.com>
2022-03-23 13:44:46 -0400

    Space formatting.

--- member_func_as_func_ptr.cpp
+++ member_func_as_func_ptr.cpp
@@ -1,44 +1,28 @@
-// Reference: https://stackoverflow.com/questions/1485983/calling-c-member-functions-via-a-function-pointer
-
 #include <iostream>
 
-class Animal
-{
-public:
+class Animal {
+  public:
+    typedef Animal *(*NewAnimalFunction)(void);
 
-    typedef Animal*(*NewAnimalFunction)(void);
-
-    virtual void makeNoise()
-    {
-        std::cout << "M00f!" << std::endl;
-    }
+    virtual void makeNoise() { std::cout << "M00f!" << std::endl; }
 };
 
-class Dog : public Animal
-{
-public:
+class Dog : public Animal {
+  public:
+    typedef void (Dog::*BarkFunction)(void);
 
-    typedef void(Dog::*BarkFunction)(void);
+    typedef Dog *(*NewDogFunction)(void);
 
-    typedef Dog*(*NewDogFunction)(void);
+    Dog() { }
 
-    Dog () {}
+    static Dog *newDog() { return new Dog; }
 
-    static Dog* newDog()
-    {
-        return new Dog;
-    }
-
-    virtual void makeNoise ()
-    {
-        std::cout << "Woof!" << std::endl;
-    }
+    virtual void makeNoise() { std::cout << "Woof!" << std::endl; }
 };
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     // Call member function via method pointer
-    Dog* pDog = new Dog ();
+    Dog *pDog = new Dog();
     Dog::BarkFunction pBark = &Dog::makeNoise;
 
     (pDog->*pBark)();
@@ -46,7 +30,7 @@ int main(int argc, char* argv[])
     // Construct instance via factory method
     Dog::NewDogFunction pNew = &Dog::newDog;
 
-    Animal* pAnimal = (*pNew)();
+    Animal *pAnimal = (*pNew)();
 
     pAnimal->makeNoise();
 
