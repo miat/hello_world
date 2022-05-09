33d49c7c84256df8a385f174072a0f96672af6cf
Miat <miat@monkeeworks.com>
2022-05-09 02:30:40 -0400

    Update big_constexpr_array.cpp

--- big_constexpr_array.cpp
+++ big_constexpr_array.cpp
@@ -2,24 +2,20 @@
 //(source: https://www.educative.io/edpresso/how-to-create-an-n-element-constexpr-array-in-cpp)
 
 #include <iostream>
-
-template <int size>
-struct BigConstArray {
-    int m_big_array[size];
-
-    constexpr BigConstArray() : m_big_array() {
-        for (int n=0; n<size; ++n) m_big_array[n] = n;
+#include <array>
+template <int SIZE>
+struct S {
+    std::array<int, SIZE> arr;
+    constexpr S() : arr{} {
+        auto i=0;
+        for (auto& e:arr) {e=i++;}
+        arr[49]=1954;
+        arr[50]=1969;
     }
-
-    const int& operator[](std::size_t idx) const {return m_big_array[idx];}
+    const int& operator[](int i) const {return arr[i];}
 };
-const int ARRAY_SIZE=100;
-
-BigConstArray<ARRAY_SIZE> big_const_array;
-
-int main () {
-
-    for(int i; i < ARRAY_SIZE; ++i)
-    std::cout << big_const_array[i] << std::endl;
-    return 69;
+constexpr S<100> s;
+int main() {
+    int i;
+    i = s[49];
 }
